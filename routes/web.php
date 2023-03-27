437601a09ebfaee1a4767adb56892df1f406ba45
Wisnu dwi haryanto <dwiwisnu540@gmail.com>
2023-03-27 16:16:05 +0700

    env, mvc, route, relationship

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,7 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
     Route::get('/todo/edit', [TodoController::class, 'edit'])->name('todo.edit');
-    
+
     Route::get('/user', [UserController::class, 'index'])->name('user.index');
 });
 
