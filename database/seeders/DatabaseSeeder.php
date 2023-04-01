0a60404700922bb90b0682b2029db0d9fb842ad7
Wisnu dwi haryanto <dwiwisnu540@gmail.com>
2023-04-01 10:16:26 +0700

    migration, database seeder, factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$AXT0rtSDq9FcVp.4CgcgHeKnQYZSMfECpA.qU8036AF9ElHl8IGuO', // password
+                'remember_token' => Str::random(10),
+                'is_admin' => true
+            ]
+        );
+        User::factory(100)->create();
+        Todo::factory(500)->create();
     }
 }
